28229e1993829bb686a6d843a7195a0131528f81
AlanPustkowiak <alan.pustkowiak@outlook.com>
2024-08-08 21:41:13 +0200

    basic service and inject into homecomponent

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Employee } from './employee.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,19 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<any> {
-    return this.http.get<any>(`${this.apiServer}/employee/all`);
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServer}/employee/all`);
+  }
+
+  public addEmployees(emplyee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServer}/employee/add`, emplyee);
+  }
+
+  public updateEmployees(emplyee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServer}/employee/update`, emplyee);
+  }
+
+  public deleteEmployees(emplyeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServer}/employee/delete/${emplyeeId}`);
   }
 }
