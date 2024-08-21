9168dced3eee8f5bf63ccd9c82ebcc3bb620455f
AlanPustkowiak <alan.pustkowiak@outlook.com>
2024-08-21 20:40:33 +0200

    another repair

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app-routes.module';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-<<<<<<< HEAD
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimationsAsync()],
-=======
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())],
->>>>>>> 10b858a62bb9a80724b8c8bbe366344818fdae24
 };
