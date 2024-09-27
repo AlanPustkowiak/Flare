f3ef4588f264de6ffb22ac44f3e6dd99b1c6bed4
AlanPustkowiak <alan.pustkowiak@outlook.com>
2024-09-27 20:37:24 +0200

    It might be too much, need to return to basics

--- src/app/employee-page/employee-page.component.ts
+++ src/app/employee-page/employee-page.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Employee } from '../employee.model';
 
 @Component({
   selector: 'app-employee-page',
   standalone: true,
-  imports: [],
+  imports: [ MatFormField, MatInputModule, MatButton, FormsModule ],
   templateUrl: './employee-page.component.html',
   styleUrl: './employee-page.component.css'
 })
 export class EmployeePageComponent {
+  public employee: Employee = {
+    name: '',
+    email: '',
+    jobTitle: '',
+    phone: '',
+    workLocation: ''
+  };
 
+  constructor() { }
+
+  public addEmployee(employee: Employee): void {
+    // employee.addEmployees(this.employee).subscribe(
+    //   (response: Employee) => {
+    //     console.log(response);
+    //   },
+    //   (error: any) => {
+    //     console.log(error);
+    //   }
+    // );
+  }
 }
