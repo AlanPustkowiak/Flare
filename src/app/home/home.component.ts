0b6ae9e4066ef4d6365dcc374268628c7dac0822
Alan Pustkowiak <alan.pustkowiak@outlook.com>
2025-03-14 18:02:08 +0100

    changes in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Employee } from '../employee/employee.model';
 import { EmployeeService } from '../employee/employee.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-home',
@@ -15,9 +16,10 @@ import { EmployeeService } from '../employee/employee.service';
 })
 export class HomeComponent implements OnInit {
   public employees: Employee[] = [];
-  displayedColumns = ['name', 'jobTitle', 'phone', 'email', 'workLocation'];
+  public searchQuery: string = '';
+  displayedColumns = ['name', 'jobTitle', 'phone', 'email', 'department' , 'workLocation', 'actions'];
 
-  constructor(private employeeService: EmployeeService, private route: Router) { }
+  constructor(private employeeService: EmployeeService, private route: Router, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.getEmployees();
@@ -29,9 +31,34 @@ export class HomeComponent implements OnInit {
         this.employees = response;
       },
       error: (error: HttpErrorResponse) => {
+        this.snackBar.open('Błąd podczas pobierania pracowników', 'Zamknij', {
+          duration: 3000
+        });
         console.log(error.message);
       }
     });
+  }
+
+  public searchEmployees(): void{
+    if(this.searchQuery.trim()){
+      this.employeeService.searchEmployees(this.searchQuery).subscribe({
+      next: (response: Employee[]) => {
+        this.employees = response;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.snackBar.open('Błąd podczas wyszukiwania pracowników', 'Zamknij', {
+          duration: 3000
+        });
+        console.log(error.message);
+      }
+    });
+    } else {
+      this.getEmployees();
+    }
+  }
+
+  public viewEmployeeDetails(id: number): void {
+    this.route.navigate(['/employee', id]);
   }
 
   public futureModal(): void {
