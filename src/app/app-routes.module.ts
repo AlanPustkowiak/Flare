0ab430f1b343ae307062243fdd7e9ef10b42a98d
Alan Pustkowiak <alan.pustkowiak@outlook.com>
2025-04-01 21:31:58 +0200

    start of the departments page

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeePageComponent } from './employee-page/employee-page.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: 'employeePage', component: EmployeePageComponent, pathMatch: 'full' },
-  { path: 'employeeDetails/:id', component: EmployeeDetailsComponent, pathMatch: 'full' }
+  { path: 'employeeDetails/:id', component: EmployeeDetailsComponent, pathMatch: 'full' },
+  { path: 'departmentList', component: DepartmentListComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+import { DepartmentService } from '../department/department.service';
+import { Department } from '../department/department.model';
 
 @Component({
   selector: 'app-department-list',
-  imports: [],
+  imports: [ MatCardModule, RouterLink ],
   templateUrl: './department-list.component.html',
   styleUrl: './department-list.component.css'
 })
 export class DepartmentListComponent {
+  departments: Department[] = [];
+  private departmentsService = inject(DepartmentService);
 
+  constructor() {
+   }
+
+   ngOnInit(): void {
+    this.getDepartments();
+   }
+
+  getDepartments(): void {
+    this.departmentsService.getDepartments().subscribe({
+      next: (response) => {
+        this.departments = response;
+      },
+      error: (error) => {
+        console.error('Error fetching departments:', error);
+      }
+    });
+  }
 }
